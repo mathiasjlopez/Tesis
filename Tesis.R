605a9c90855d013770b11b2569e2baafd3c9eeb3
Mathias Lopez <mathias.j.lopez@gmail.com>
2024-08-17 14:33:47 -0300

    Descripción de los cambios

--- Tesis.R
+++ Tesis.R
@@ -3,32 +3,54 @@ rm(list = ls())
 install.packages("glmmTMB")
 install.packages("tidyverse")
 install.packages("matrix")
+install.packages("DHARMa")
+install.packages("psych")
 
 library(lme4)
 library(glmmTMB)
 library(tidyverse)
 library(ggplot2)
-
+library(DHARMa)
+library(psych)
 
 ENFR_temporal <- read.csv("C:/Tesis/Datos/EsNsFR.csv", header = T, sep = ",", dec = ".")
 
 NBI_CNA <- read.csv("C:/Tesis/Datos/CNA + NBI/NBI_Prov_Total_Y_CNA.csv", header = T, sep = ",", dec = ".")
 
 
-# Pasamos a factor la variable Año:
+#_______________________________________________________________________________
+
+# Algunos arreglos en el dataset:
+
+# Pasamos a factor todas las variables:
+
 ENFR_temporal$Año_Edicion <- factor(ENFR_temporal$Año_Edicion)
 
-# Pasamos a factor la variable Quintil de ingreso:
+ENFR_temporal$Provincia <- factor(ENFR_temporal$Provincia)
+
+ENFR_temporal$Sexo <- factor(ENFR_temporal$Sexo)
+
+ENFR_temporal$Nivel_de_instrucción <- factor(ENFR_temporal$Nivel_de_instrucción)
+
+ENFR_temporal$Sit_laboral <- factor(ENFR_temporal$Sit_laboral)
+
+ENFR_temporal$Cobertura_salud <- factor(ENFR_temporal$Cobertura_salud)
+
+ENFR_temporal$Indice_NBI_hogar_dic <- factor(ENFR_temporal$Indice_NBI_hogar_dic)
 
 ENFR_temporal$Quintil_ingresos <- factor(ENFR_temporal$Quintil_ingresos)
+
+ENFR_temporal$Promedio_fv_Diario_Dic <- factor(ENFR_temporal$Promedio_fv_Diario_Dic)
+
 
 # Pasamos los casos de "No_cumple" -> 0 y "Cumple" -> 1 de la variable "Promedio_fv_Diario_Dic":
 table(ENFR_temporal$Promedio_fv_Diario_Dic)
 
 ENFR_temporal <-ENFR_temporal %>%
   filter( Promedio_fv_Diario_Dic != "<NA>") %>% 
-  mutate( Consumo_FyV = ifelse(Promedio_fv_Diario_Dic == "Cumple", 1, 0))
+  mutate( Cumple_No_Cumple_FyV = ifelse(Promedio_fv_Diario_Dic == "Cumple", 1, 0))
 
+ENFR_temporal$Cumple_No_Cumple_FyV<- factor(ENFR_temporal$Cumple_No_Cumple_FyV)
 
 ###_____________________________________________________________________________
 
@@ -127,34 +149,73 @@ ggplot( ENFR_temporal, aes(x = Provincia))+
   geom_bar( fill = "skyblue", color = "blue")+
   theme( axis.text.x = element_text(angle = 90, hjust = 0.5))
 
+#---
+# Usamos la función describeBy()(para variables numericas) del paquete psych, para generar estadísticas descriptivas agrupadas según una o más variables. Es muy útil cuando quieres obtener resúmenes estadísticos de un conjunto de datos dividido por grupos.
+
+sum(is.na(ENFR_temporal$Cumple_No_Cumple_FyV))
+
+describeBy(ENFR_temporal$Promedio_fyv_dia, group = ENFR_temporal$Año_Edicion)
+
 #_______________________________________________________________________________
-names(ENFR_temporal)
 
-# Modelos simples:
 
+### Modelos simples ###
+ str(ENFR_temporal)
 
 # Modelos simple con Año como V E F mas Provincia como V E A:
- m1a <- glmer( Consumo_FyV ~ Año_Edicion + (1|Provincia ) , ENFR_temporal, family = binomial)
+ m1a <- glmer( Cumple_No_Cumple_FyV ~ Año_Edicion + (1|Provincia ) , ENFR_temporal, family = binomial)
  summary(m1a)
  anova(m1a)
  
+simulationOutput <- simulateResiduals(fittedModel = m1a, plot = T)
+ 
+ 
  # Modelos simple con Nivel_de_instrucción como V E F mas Provincia como V E A: 
- m1b <- glm( Consumo_FyV ~ Nivel_de_instrucción + (1|Provincia ), ENFR_temporal, family = binomial)
+table(ENFR_temporal$Nivel_de_instrucción) 
+m1b <- glmer( Cumple_No_Cumple_FyV ~ Nivel_de_instrucción + (1 | Provincia ), ENFR_temporal, family = binomial)
+ summary(m1b)
+ anova(m1b) 
+ 
+ simulationOutput <- simulateResiduals(fittedModel = m1b, plot = T)
  
  # Modelos simple con Edad como V E F mas Provincia como V E A:
- m1c <- glm( Consumo_FyV ~  Edad, ENFR_temporal + (1|Provincia ), family = binomial)
+ m1c <- glmer( Cumple_No_Cumple_FyV ~  Edad, ENFR_temporal + (1|Provincia ), family = binomial)
+ summary(m1b)
+ anova(m1b)
+ 
+ simulationOutput <- simulateResiduals(fittedModel = m1c, plot = T)
  
  # Modelos simple con Sexo como V E F mas Provincia como V E A:
- m1d <- glm( Consumo_FyV ~  Sexo, ENFR_temporal + (1|Provincia ), family = binomial)
+ m1d <- glmer( Cumple_No_Cumple_FyV ~  Sexo, ENFR_temporal + (1|Provincia ), family = binomial)
+ summary(m1d)
+ anova(m1d)
+ 
+ simulationOutput <- simulateResiduals(fittedModel = m1d, plot = T)
  
  # Modelos simple con Sit_laboral como V E F mas Provincia como V E A:
- m1e <- glm( Consumo_FyV ~  Sit_laboral + (1|Provincia ), ENFR_temporal, family = binomial)
+ m1e <- glmer( Cumple_No_Cumple_FyV ~  Sit_laboral + (1|Provincia ), ENFR_temporal, family = binomial)
+ summary(m1d)
+ anova(m1d)
+ 
+ simulationOutput <- simulateResiduals(fittedModel = m1e, plot = T)
  
  # Modelos simple con Cobertura_salud como V E F mas Provincia como V E A:
- m1f <- glm( Consumo_FyV ~  Cobertura_salud + (1|Provincia ), ENFR_temporal, family = binomial)
+ m1f <- glmer( Cumple_No_Cumple_FyV ~  Cobertura_salud + (1|Provincia ), ENFR_temporal, family = binomial)
+ summary(m1f)
+ anova(m1f)
+ 
+ simulationOutput <- simulateResiduals(fittedModel = m1f, plot = T)
  
  # Modelos simple con Quintil_ingresos como V E F mas Provincia como V E A:
- m1g <- glm( Consumo_FyV ~  Quintil_ingresos + (1|Provincia ), ENFR_temporal, family = binomial)
+ m1g <- glmer( Cumple_No_Cumple_FyV ~  Quintil_ingresos + (1|Provincia ), ENFR_temporal, family = binomial)
+ summary(m1g)
+ anova(m1g)
+ 
+ simulationOutput <- simulateResiduals(fittedModel = m1g, plot = T)
  
  # Modelos simple con Año como V E F mas Provincia como V E A:
- m1h <- glm( Consumo_FyV ~  + (1|Provincia ), ENFR_temporal, family = binomial)
+ m1h <- glmer( Cumple_No_Cumple_FyV ~  + (1|Provincia ), ENFR_temporal, family = binomial)
+ 
+ 
+ simulationOutput <- simulateResiduals(fittedModel = m1h, plot = T)
+ 
